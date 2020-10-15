2832b9030a4d84d205d7b8e0857a1b0571b40d40
bayu <bayuyudo20@gmail.com>
2020-10-15 21:38:41 +0700

    menambahkan menu

--- menu-service/main.go
+++ menu-service/main.go
@@ -5,14 +5,14 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/Bayu200720/DTS-microservice.git/auth/handler"
+	"github.com/Bayu200720/DTS-microservice/menu-service/handler"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
 
-	router.Handle("/admin-auth", http.HandlerFunc(handler.ValidateAuth))
+	router.Handle("/add-menu", http.HandlerFunc(handler.AddMenu))
 
 	fmt.Printf("Auth service listen on :8001")
 	log.Panic(http.ListenAndServe(":8001", router))
